f8739da29ac85b35b5c381a3538acf5807794e5a
Erik Magnusson <106183293+Sklipsy@users.noreply.github.com>
2024-01-03 21:56:42 +0100

    Add files via upload
    Förhoppningsvis gick det bra. :D

---
+++ TO/TicketOffice.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Mail;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+//static:<- It means Main Method can be called without an object.
+//public:<- It is access modifiers which means the compiler can execute this from anywhere.
+//void:  <- The void method doesn’t return anything..
+//Main():<- It is the configured name of the Main method.
+//String <- [] args: For accepting the zero-indexed command line arguments. args is the user-defined name.
+
+namespace Ticket_Office_Assignment
+{
+    class TicketOffice//class
+    {
+
+        public static decimal TaxCalculator(int price)// metod // nyckel-ord
+        {
+
+            decimal tax = (decimal)((1 - 1 / 1.06) * price);
+            return tax;
+
+        }
+
+        public static int PriceSetter(int age, enum place)// metod // nyckel-ord
+
+        {
+
+            int price = 0;
+            if (age <= 11)
+            {
+                price = (place=="Standing")? 26 : 51;//osäker
+            }
+            else if (age >= 12 && age <= 64)
+            {
+                price = (place== "Standing") ? 110 : 170;
+            }
+            else if (age>=65)
+            {
+                price = 60;
+            
+            return price;
+
+        }
+        public static int TicketNumberGenerator()// metod // nyckel-ord
+        {
+            int x = new Random().Next(1, 8001);
+            return x;
+        }
+
+        public static void Main(string[]args)//main sista nyckel + värden och cw/consol+ parameter värden 
+        {
+            Console.Write("Enter your age: ");
+
+            int age = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter 'Standing' or 'Seated' for your ticket type: ");
+
+            string? place = Console.ReadLine();
+
+            int price = PriceSetter(age, place);
+            Console.WriteLine($"Total Price: {price} sek");
+
+            decimal tax = TaxCalculator(price);
+            Console.WriteLine($"Tax: {tax:F2} sek");
+            int ticketNumber = TicketNumberGenerator();
+
+            Console.WriteLine($"Ticket Number please!: {ticketNumber}");
+
+        }
+      public class enum{
+            Seated,Standing
+        }
+    }
+}
+
+
---
+++ TO/TicketOffice/TicketOffice/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+
+namespace Ticket_Office_Assignment
+{
+    class TicketOffice_Assignment
+    {
+        
+
+        public static int TicketNumberGenerator()
+        {
+            int x = new Random().Next(1, 8001);
+            return x;
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter your age: ");
+            int age = int.Parse(Console.ReadLine()!);
+
+            Console.Write("Enter 'Standing' or 'Seated' for your ticket type: ");
+            string? place = Console.ReadLine();
+            Ticket ticket;
+            Enum.TryParse(place, out ticket);
+
+            Tickets userTicket = new Tickets(age, ticket);
+
+            int price = userTicket.Price();//ändrad
+            Console.WriteLine($"Total Price: {price} sek");
+
+            decimal Tax = userTicket .Tax();
+            Console.WriteLine($"Tax: {Tax:F2} sek");
+
+            int ticketNumber = TicketNumberGenerator();
+            Console.WriteLine($"Ticket Number please!: {ticketNumber}");
+
+            Tickets ticketOb = new Tickets(24, Ticket.Standing);//nytt obj ticketOb
+            
+            var tax = ticketOb.Tax();
+            ticketOb.Place = Ticket.Seated;
+            int priceVal = ticketOb.Price();//.price i slutet
+            TicketSalesManager ticketSalesManager = new TicketSalesManager();//parantes sista
+            ticketSalesManager.Addticket(ticketOb);
+            var x= ticketSalesManager.AmountOfTickets();
+            Console.WriteLine($"totala antalet biljetter är{x}");
+            var z = ticketSalesManager.SalesTotal();
+            Console.WriteLine($"totala beloppet är{z}");
+
+            ticketSalesManager.RemoveTicket(ticketOb);//ticketOb
+
+            
+        }
+    }
+    public enum Ticket
+    {
+        Seated, Standing
+    }
+
+    public class Tickets
+    {
+        public decimal Tax()
+        {
+            int price = Price();
+            decimal tax = (decimal)((1 - 1 / 1.06) * price);
+            return tax;
+        }
+        public int Age { get; set; }
+        public Ticket Place { get; set; }
+        public int Number { get; set; }
+
+        public Tickets(int age, Ticket place)
+        {
+            this.Age = age;
+            this.Place = place;
+            this. Number = TicketOffice_Assignment.TicketNumberGenerator();
+        }
+        public int Price()
+        {
+            int price = 0;
+            if (Age <= 11)//föregående age till Age
+            {
+                price = Place == Ticket.Standing ? 26 : 51;// ändrar place till Place.
+            }
+            else if (Age >= 12 && Age <= 64)// Lika dant ifrån age till Age
+            {
+                price = Place == Ticket.Standing ? 110 : 170;
+            }
+            else if (Age >= 65)
+            {
+                price = 60;
+            }
+            return price;
+        }       
+    }
+        public class TicketSalesManager
+    {
+        private List<Tickets> ticketList = new List<Tickets>();
+        public Tickets Addticket(Tickets ToAdd ) //skjuter in i listan(Tikets)
+        { //ToAdd (namn)
+            ticketList.Add(ToAdd);//toAdd
+            return ToAdd;
+        }
+        public bool RemoveTicket(Tickets ticket)//return bool ist för void
+        {
+            try
+            {
+                ticketList.RemoveAll(v => v.Number == ticket.Number);//copy pasta i trycatch 
+                return true;//throw new exceptions ();
+            }
+            catch { return false; }
+        }
+        public decimal SalesTotal()
+        {
+            int totalPrice = 0;
+            
+            foreach (Tickets ticket in ticketList)
+            {
+               totalPrice += ticket.Price();//+=       
+            }
+            return (decimal) totalPrice; //decimal ()
+        }
+        public int AmountOfTickets()
+        {
+            return ticketList.Count;
+        }
+    }
+}
